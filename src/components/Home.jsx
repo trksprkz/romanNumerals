6470929f5595e1e6c41d8f74460600aae6f9867d
Issac Davis <trksprkz@gmail.com>
2023-01-12 21:49:37 -0600

    button and justify-around

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,35 +39,25 @@ function Home() {
           Convert any number to Roman Numerals
         </h1>
         <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
-          Sub-hero message, not too long and not too short. Make it just right!
+          What are you waiting for?
         </p>
 
-        <form class="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
           <div class="mb-4">
-            <label class="block text-blue-300 py-2 font-bold mb-2" for="emailaddress">
-              Signup for our newsletter
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-              id="emailaddress"
-              type="text"
-              placeholder="you@somewhere.com"
-            />
+
           </div>
 
-          <div class="flex items-center justify-between pt-4">
+          <div class="flex justify-around items-center pt-4">
             <button
               class="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
               type="button"
             >
-              Sign Up
+              Check It Out
             </button>
           </div>
-        </form>
-      </div>
 
+      </div>
       <div class="w-full xl:w-3/5 p-12 overflow-hidden">
-        <img class="mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6" src="macbook.svg" />
+        <img class="flex mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6" src="macbook.svg" />
       </div>
 
       <div class="mx-auto md:pt-16">
@@ -80,11 +70,6 @@ function Home() {
         </div>
       </div>
 
-      <div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
-        <a class="text-gray-500 no-underline hover:no-underline" href="#">&copy; App 2020</a>
-        - Template by
-        <a class="text-gray-500 no-underline hover:no-underline" href="https://www.tailwindtoolbox.com">TailwindToolbox.com</a>
-      </div>
     </div>
   </div>
 </body>
