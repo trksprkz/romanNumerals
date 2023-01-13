9f600000315c88fdb6df3fb86d317a27d0653011
Issac Davis <trksprkz@gmail.com>
2023-01-12 22:32:35 -0600

    reused home component for convert page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -55,6 +55,7 @@ function Home() {
           </div>
 
       </div>
+
       <div class="w-full xl:w-3/5 p-12 overflow-hidden">
         <img class="flex mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6" src="macbook.svg" />
       </div>
